b58b9a32d4a583fbeeb946a124dd7b23f333aa1e
ChaoGaoUCR <cgao037@ucr.edu>
2025-03-18 00:21:17 +0000

    modified:   utils/adjGraphAddWeights.C

--- utils/adjGraphAddWeights.C
+++ utils/adjGraphAddWeights.C
@@ -4,7 +4,7 @@
 // Permission is hereby granted, free of charge, to any person obtaining a
 // copy of this software and associated documentation files (the
 // "Software"), to deal in the Software without restriction, including
-// without limitation the rights (to use, copy, modify, merge, publish,
+// without limitation the rights to use, copy, modify, merge, publish,
 // distribute, sublicense, and/or sell copies of the Software, and to
 // permit persons to whom the Software is furnished to do so, subject to
 // the following conditions:
@@ -20,7 +20,6 @@
 // OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-// Adds a random integer weight in [1...log(n)] to each edge
 #include <math.h>
 #include "graphIO.h"
 #include "parseCommandLine.h"
@@ -41,43 +40,42 @@ int parallel_main(int argc, char* argv[]) {
   
   intT* Weights = newA(intT,m);
 
-  intT maxEdgeLen = log2(n);
-  intT* Choices = newA(intT,2*maxEdgeLen);
-  
-  parallel_for (intT i=0;i<maxEdgeLen;i++){
-    Choices[2*i] = i+1;
-    Choices[2*i+1] = i+1;
-    //Choices[2*i+1] = -(i/10)-1;
-  }
+  // === 修改部分：根据 (src + dst) % 16 + 1 生成权重 ===
+  parallel_for (long v=0; v<n; v++) {
+    intT degree = G.V[v].degree;
+    intT* neighbors = G.V[v].Neighbors;
+    intT offset = neighbors - (G.allocatedInplace + 2 + n);
 
-  parallel_for (long i=0;i<m;i++) {
-    Weights[i] = Choices[hashInt((uintT)i) % (2*maxEdgeLen)];
-    //if(i%1000==0 && Weights[i] < 0) Weights[i]*=-1;
+    for (intT j = 0; j < degree; j++) {
+      intT i = offset + j;
+      intT dst = neighbors[j];
+      Weights[i] = (v + dst) % 16 + 1;
+    }
   }
-  free(Choices);
-  
+  // =======================================
+
   wghVertex<intT>* WV = newA(wghVertex<intT>,n);
-  intT* Neighbors_start = G.allocatedInplace+2+n;
+  intT* Neighbors_start = G.allocatedInplace + 2 + n;
 
   parallel_for(long i=0;i<n;i++){
     WV[i].Neighbors = G.V[i].Neighbors;
     WV[i].degree = G.V[i].degree;
     intT offset = G.V[i].Neighbors - Neighbors_start;
-    WV[i].nghWeights = Weights+offset;
+    WV[i].nghWeights = Weights + offset;
   }
 
-  //symmetrize
+  // symmetrize 权重保持一致
   parallel_for(long i=0;i<n;i++){
     parallel_for(long j=0;j<WV[i].degree;j++){
       uintT ngh = WV[i].Neighbors[j];
       if(ngh > i) {
-	for(long k=0;k<WV[ngh].degree;k++) {
-	  uintT ngh_ngh = WV[ngh].Neighbors[k];
-	  if(ngh_ngh == i) {
-	    WV[i].nghWeights[j] = WV[ngh].nghWeights[k];
-	    break;
-	  }
-	}
+        for(long k=0;k<WV[ngh].degree;k++) {
+          uintT ngh_ngh = WV[ngh].Neighbors[k];
+          if(ngh_ngh == i) {
+            WV[i].nghWeights[j] = WV[ngh].nghWeights[k];
+            break;
+          }
+        }
       }
     }
   }
